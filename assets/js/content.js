5e7cc801dbd9a7af2f6ba311a80e4e764bef612e
Thomas Rousseau <thomas.rousseau@proton.me>
2025-08-28 21:06:09 +0200

    chore(*) add linter

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,44 +1,40 @@
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-    if (request.action === 'checkAll') {
-        await checkAllElements();
-    }
-    if (request.action === 'uncheckAll') {
-        await uncheckAllElements();
-    }
-});
+  if (request.action === 'checkAll') {
+    await checkAllElements()
+  }
+  if (request.action === 'uncheckAll') {
+    await uncheckAllElements()
+  }
+})
 
 async function checkAllElements() {
-    const elements = document.querySelectorAll('label.js-reviewed-toggle.color-border-muted');
+  const elements = document.querySelectorAll('label.js-reviewed-toggle.color-border-muted')
 
-    for (let i = 0; i < elements.length; i++) {
-        await clickElement(elements[i]);
-        await new Promise(resolve => setTimeout(resolve, 150));
-    }
-
-    console.log("All files are checked !");
+  for (let i = 0; i < elements.length; i++) {
+    await clickElement(elements[i])
+    await new Promise((resolve) => setTimeout(resolve, 150))
+  }
 }
 
 async function uncheckAllElements() {
-    const elements = document.querySelectorAll('label.js-reviewed-toggle.color-border-accent');
+  const elements = document.querySelectorAll('label.js-reviewed-toggle.color-border-accent')
 
-    for (let i = 0; i < elements.length; i++) {
-        await clickElement(elements[i]);
-        await new Promise(resolve => setTimeout(resolve, 150));
-    }
-
-    console.log("All files are unchecked !");
+  for (let i = 0; i < elements.length; i++) {
+    await clickElement(elements[i])
+    await new Promise((resolve) => setTimeout(resolve, 150))
+  }
 }
 
 async function clickElement(element) {
-    return new Promise(resolve => {
-        if (element) {
-            element.click();
-            resolve();
-        } else {
-            setTimeout(async () => {
-                await clickElement(element);
-                resolve();
-            }, 50);
-        }
-    });
+  return new Promise((resolve) => {
+    if (element) {
+      element.click()
+      resolve()
+    } else {
+      setTimeout(async () => {
+        await clickElement(element)
+        resolve()
+      }, 50)
+    }
+  })
 }
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const checkAllBtn = document.getElementById('check-all-files-btn');
-    const uncheckAllBtn = document.getElementById('uncheck-all-files-btn');
+document.addEventListener('DOMContentLoaded', function () {
+  const checkAllBtn = document.getElementById('check-all-files-btn')
+  const uncheckAllBtn = document.getElementById('uncheck-all-files-btn')
 
-    checkAllBtn.addEventListener('click', function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'checkAll' });
-        });
-    });
+  checkAllBtn.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'checkAll' })
+    })
+  })
 
-    uncheckAllBtn.addEventListener('click', function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'uncheckAll' });
-        });
-    });
-});
+  uncheckAllBtn.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'uncheckAll' })
+    })
+  })
+})
